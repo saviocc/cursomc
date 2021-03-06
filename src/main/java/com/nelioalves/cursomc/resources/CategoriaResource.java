f014b3380e6abeee500b3bfe8557d14581d345fd
Savio Cavalcante <saviocc@gmail.com>
2021-03-06 20:38:19 -0300

    Banco de dados H2

--- src/main/java/com/nelioalves/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/nelioalves/cursomc/resources/CategoriaResource.java
@@ -17,12 +17,12 @@ public class CategoriaResource {
 	@RequestMapping(method=RequestMethod.GET)
 	public List<Categoria> listar() {
 		
-		Categoria cat1 = new Categoria(1, "Informática");
-		Categoria cat2 = new Categoria(2, "Escritório");
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
 		
 		List<Categoria> lista = new ArrayList<>();
 		lista.add(cat1);
-		lista.add(cat1);
+		lista.add(cat2);
 
 		return lista;
 	}
