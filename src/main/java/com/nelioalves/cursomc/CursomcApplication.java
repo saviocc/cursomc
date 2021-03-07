64c19f0823bc6cfcaf12aab2c0f4b3948740963c
Savio Cavalcante <saviocc@gmail.com>
2021-03-07 15:49:38 -0300

    Produto e associações

--- src/main/java/com/nelioalves/cursomc/CursomcApplication.java
+++ src/main/java/com/nelioalves/cursomc/CursomcApplication.java
@@ -1,6 +1,7 @@
 package com.nelioalves.cursomc;
 
 import java.util.Arrays;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -8,13 +9,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.nelioalves.cursomc.domain.Categoria;
+import com.nelioalves.cursomc.domain.Produto;
 import com.nelioalves.cursomc.repositories.CategoriaRepository;
+import com.nelioalves.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
 	
 	@Autowired
 	CategoriaRepository categoriaRepository;
+	ProdutoRepository produtoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -22,11 +26,25 @@ public class CursomcApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		Categoria cat1 = new Categoria(null, "Informática");
-		Categoria cat2 = new Categoria(null, "Escritório");
+		Optional<Categoria> cat1 = categoriaRepository.findById(1);
+		Optional<Categoria> cat2 = categoriaRepository.findById(2);
 		
-		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
-		
+		//categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+
+		/*
+		 * Produto prd1 = new Produto(null, "Computador", 2000.); Produto prd2 = new
+		 * Produto(null, "Impressora", 800.); Produto prd3 = new Produto(null, "mouse",
+		 * 80.);
+		 * 
+		 * cat1.get().getProdutos().addAll(Arrays.asList(prd1, prd2, prd3));
+		 * cat2.get().getProdutos().addAll(Arrays.asList(prd2));
+		 * 
+		 * prd1.getCategorias().addAll(Arrays.asList(cat1.get()));
+		 * prd2.getCategorias().addAll(Arrays.asList(cat1.get(), cat2.get()));
+		 * prd3.getCategorias().addAll(Arrays.asList(cat1.get()));
+		 * 
+		 * produtoRepository.saveAll(Arrays.asList(prd1, prd2, prd3));
+		 */
 	}
 
 }
