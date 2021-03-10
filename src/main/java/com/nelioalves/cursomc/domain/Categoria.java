4264068364484a248a68daf80d77c3cede5c9f1e
Savio Cavalcante <saviocc@gmail.com>
2021-03-10 12:16:33 -0300

    Iserindo categoria

--- src/main/java/com/nelioalves/cursomc/domain/Categoria.java
+++ src/main/java/com/nelioalves/cursomc/domain/Categoria.java
@@ -23,7 +23,7 @@ public class Categoria implements Serializable {
 	private Integer id;
 	private String nome;
 	
-	@JsonManagedReference
+	//@JsonManagedReference
 	@ManyToMany(mappedBy = "categorias")
 	private List<Produto> produtos = new ArrayList<>();
 	
